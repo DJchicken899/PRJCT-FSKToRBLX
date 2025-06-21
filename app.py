3b0ed66bfc6dfc55cbe0e921933119eedffa28a1
DJchicken899 <laceywilley@gmail.com>
2025-06-21 15:42:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"status": "online"})
+
+@app.route('/status')
+def status():
+    return jsonify({"triggered": True})
+
+if __name__ == '__main__':
+    app.run()
